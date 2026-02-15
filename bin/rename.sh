64f693e12ba10e7d2f1dd203a076f4e3eef2d8ff
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2026-02-15 22:50:45 +0100

    put default dir for rename

--- bin/rename.sh
+++ bin/rename.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Get the directory of this script
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-# Path to the rename.py script
 PYTHON_SCRIPT="$SCRIPT_DIR/../lib/rename.py"
 
-# Check if argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <directory>"
-    exit 1
+DIR="$1"
+if [ -z "${DIR}" ]; then
+    DIR=${HOME}/Downloads/output
 fi
 
-# Run the Python script with the provided directory
-python3 "$PYTHON_SCRIPT" "$1"
+python3 "$PYTHON_SCRIPT" "$DIR"
